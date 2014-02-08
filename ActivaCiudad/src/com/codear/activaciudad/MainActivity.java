b09f608b137eb55e6b31a286575181d7deba7d48
sebastian <t_o_wi@hotmail.com>
2014-02-08 01:03:13 -0600

    Cambiando de AndroidStudio a Eclipse ADT
    Cambiando el proyecto hecho en AndroidEstudio por uno hecho con Eclipse
    ADT ademas de otras cosas
    •Splash
    •Login

---
+++ ActivaCiudad/src/com/codear/activaciudad/MainActivity.java
@@ -0,0 +1,40 @@
+package com.codear.activaciudad;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	private static final long SPLASH_SCREEN_DELAY = 2000;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		TimerTask task = new TimerTask() {
+			
+			@Override
+			public void run() {
+				Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		};
+		Timer timer = new Timer();
+		timer.schedule(task, SPLASH_SCREEN_DELAY);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
