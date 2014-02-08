9e08e531c04c1c6d96aa6637c103c2460bfa9c1d
Rodrigo Moreno <rod.netsecurity@gmail.com>
2014-02-08 13:41:46 -0600

    Revert 0afc39a..16d2397
    This rolls back to commit 0afc39a3080b15baa2f94a9a01b9dc421991d8f6.

--- ActivaCiudad/src/com/codear/activaciudad/LoginActivity.java
+++ ActivaCiudad/src/com/codear/activaciudad/LoginActivity.java
@@ -2,19 +2,13 @@ package com.codear.activaciudad;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Typeface;
-import android.util.Log;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends Activity {
 	
 	TextView txtIngresa, txtMail, txtPass, txtUserNew;
-	Button btnComenzar, btnRegistrar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +20,6 @@ public class LoginActivity extends Activity {
 		txtPass = (TextView) findViewById(R.id.pass);
 		txtUserNew = (TextView) findViewById(R.id.usuarioNuevo);
 		
-		btnComenzar = (Button) findViewById(R.id.btnEntrar);
-		btnRegistrar = (Button) findViewById(R.id.btnRegistrarse);
-		
 		Typeface faceNormal = Typeface.createFromAsset(getAssets(), "fonts/larkenormal.ttf");
 		Typeface faceBold = Typeface.createFromAsset(getAssets(), "fonts/larkebold.ttf");		
 		txtIngresa.setTypeface(faceBold);
@@ -36,44 +27,6 @@ public class LoginActivity extends Activity {
 		txtPass.setTypeface(faceNormal);
 		txtUserNew.setTypeface(faceNormal);
 		
-		btnComenzar.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				Log.w("LoginActivity", "boton comenzar");
-				login();
-			}
-		});
-		
-		btnRegistrar.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Log.w("LoginActivity", "boton registrar");
-				registrar();
-			}
-		});
-		
-	}
-
-	protected void login() {
-		Log.w("LoginActivity", "Login");
-		if (validacion()) {
-			Log.w("Validacion", "validacion "+validacion());
-		}
-		Intent intent = new Intent(getApplicationContext(), LevantamientoActivity.class);
-		startActivity(intent);
-	}
-	
-
-	protected void registrar() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private boolean validacion() {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
